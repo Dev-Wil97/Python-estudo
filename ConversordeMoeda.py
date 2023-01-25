1ad2fa5fb94a08a32a9ea978e244fd5e3bfa5c8b
DevWKs <114428486+DevWKs@users.noreply.github.com>
2023-01-25 12:05:59 -0300

    Create ConversordeMoeda.py

---
+++ ConversordeMoeda.py
@@ -0,0 +1,5 @@
+# barn-culo-estudioso
+
+real = float(input('Quanto de dinheiro você tem na carteira? R$ '))
+dolar = real / 5.11
+print('Com R${:.2f} você pode comprar US{:.2f}'.format(real, dolar))
