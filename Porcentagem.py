fdd843b16e6fe4b3d3de971d74a3b06482f97986
Wilkias Freitas <114428486+DevWKs@users.noreply.github.com>
2023-01-26 16:36:46 -0300

    Create Porcentagem.py

---
+++ Porcentagem.py
@@ -0,0 +1,8 @@
+# barn-culo-estudioso
+Programas básicos de aprendizado :)
+
+#Calculando valores com 5% de desconto.
+
+preço = float(input('Qual o valor do produto ? R$'))
+novo = preço - (preço * 5 / 100 )
+print('O produto que custava R${:.2f}, na promoção com desconto de 5% vai custar R${:.2f}'.format( preço, novo))
