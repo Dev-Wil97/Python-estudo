624b40d87d358b52a07779899c555421ea778e60
Wilkias Freitas <114428486+DevWKs@users.noreply.github.com>
2023-01-26 17:04:23 -0300

    Create Temperatura.py

---
+++ Temperatura.py
@@ -0,0 +1,8 @@
+# barn-culo-estudioso
+Programas básicos de aprendizado :)
+
+# Convertendo graus celsius para graus Fahrenheit.
+
+c = float(input('Informe a temperatura em C:'))
+f = (9 * temp / 5) + 32
+print('A temperatura de {}C , corresponde a {}F ! '.format(temp , f))
