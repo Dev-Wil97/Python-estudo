dacbb28471f97368b45f3174270dfc6283052ee7
Wilkias Freitas <114428486+DevWKs@users.noreply.github.com>
2023-01-26 16:52:22 -0300

    Create AumentoDeSalario.py

---
+++ AumentoDeSalario.py
@@ -0,0 +1,8 @@
+# barn-culo-estudioso
+Programas básicos de aprendizado :)
+
+  Um novo salário com 15% de aumento 
+  
+real = float(input('Qual é o salário do funcionario? R$'))
+novo = real + (real * 15 / 100)
+print('Um funcionario que ganhava R${:.2f}, com 15% de aumento passa a receber R${:.2f}.'.format(real , novo))
