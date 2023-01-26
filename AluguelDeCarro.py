7ba8ac2d02c4772b9197c18213aa7ee477eedefb
Wilkias Freitas <114428486+DevWKs@users.noreply.github.com>
2023-01-26 17:39:12 -0300

    Create AluguelDeCarro.py

---
+++ AluguelDeCarro.py
@@ -0,0 +1,9 @@
+# barn-culo-estudioso
+Programas básicos de aprendizado :)
+
+Calculando o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.
+
+dias = int(input('Quantos dias foi alugado? '))
+km = float(input('Quantos KM rodados? '))
+pago = (dias * 60) + (km * 0.15)
+print('O total a pagar é de {:.2f}'.format(pago) 
