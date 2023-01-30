94de410df856318a3ee973d4f4a6f5b5cb29f35f
Wilkias Freitas <114428486+DevWKs@users.noreply.github.com>
2023-01-30 12:06:23 -0300

    Update Modulos.py

--- Modulos.py
+++ Modulos.py
@@ -8,3 +8,15 @@ from math import floor
 num  = float (input('Digite um numero: '))
 porção = floor(num)
 print (' O numero {} tem a parte inteira {}'.format(num,porção))
+
+'''Usando o modulo "Trunc" que faz o corte na parte inteira do numero'''
+
+Import math
+num  = float (input('Digite um numero: '))
+print (' O numero {} tem a parte inteira {}'.format(num, math.trunc(num)))
+
+
+Outra maneira simples ja interna 
+
+num  = float (input('Digite um numero: '))
+print (' O numero {} tem a parte inteira {}'.format(num, int(num)))
