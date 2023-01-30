7c6ab29fb136fb62cf16a012874d2c24bcca2d07
Wilkias Freitas <114428486+DevWKs@users.noreply.github.com>
2023-01-30 11:35:56 -0300

    Create Modulos.py

---
+++ Modulos.py
@@ -0,0 +1,10 @@
+# barn-culo-estudioso
+Programas básicos de aprendizado :)
+
+Quebrando Números usando Modúlos From/Import
+
+from math import floor
+
+num  = float (input('Digite um numero: '))
+porção = floor(num)
+print (' O numero {} tem a parte inteira {}'.format(num,porção))
