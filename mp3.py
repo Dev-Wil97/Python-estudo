9ac061bf8b7bfe19a8aa991a5ab9025c657bf827
Wilkias Freitas <114428486+DevWKs@users.noreply.github.com>
2023-02-02 21:28:42 -0300

    Create mp3.py

---
+++ mp3.py
@@ -0,0 +1,10 @@
+# barn-culo-estudioso
+Programas básicos de aprendizado :)
+
+# Tocando uma Musica 
+
+from pygame import mixer
+mixer.init()
+mixer.music.load('haha.mp3')
+mixer.music.play()
+x = input('Digite algo para parar...')
